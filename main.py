c633d3c06e7a4cb32863e771e37c916556f77615
AKethidi <48158707+AKethidi@users.noreply.github.com>
2026-04-20 12:28:13 -0700

    comments for code split

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #main file 
+
+
+# import from queue import Queue
+from urllib.parse import urljoin, urldefrag
+from bs4 import BeautifulSoup
+import requests
+
+#advithi
+
+# load the urls from seed list into frontier
+
+#while the frontier isn't empty & max_count < threshold,
+
+#sabrina
+
+# grab each url from frontier & parse through & save html file
+# add all the links to the frontier
